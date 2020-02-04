00377fbc9c300a4a394da2712c82e50927e97550
CapitaineToinon <asutter@wideagency.com>
2020-02-04 14:14:04 +0100

    Workaround for v-on warning

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -2,6 +2,8 @@
   <div class="categories">
     <b-menu-list label="Categories">
       <b-menu-item
+        tag="router-link"
+        :to="{}"
         v-for="(category, i) in categories"
         :key="i"
         icon="information-outline"
