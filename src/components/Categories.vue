c20ae08d8eb91b3c99310548d0dcd556a2596a2d
CapitaineToinon <asutter@wideagency.com>
2020-03-09 16:05:04 +0100

    Removed router-link

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -2,8 +2,6 @@
   <div class="categories">
     <b-menu-list label="Categories">
       <b-menu-item
-        tag="router-link"
-        :to="{}"
         v-for="(category, i) in categories"
         :key="i"
         icon="information-outline"
