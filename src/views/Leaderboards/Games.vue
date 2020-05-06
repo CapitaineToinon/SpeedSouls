8c946727806069165f42afdf248cb0ec123e7dfb
CapitaineToinon <antoinesutter@gmail.com>
2020-05-06 14:03:53 +0200

    Switch games to grid layout

--- src/views/Leaderboards/Games.vue
+++ src/views/Leaderboards/Games.vue
@@ -1,35 +1,38 @@
 <template>
-  <div class="container flex flex-row flex-wrap">
-    <div class="title p-2 w-full">
+  <div class="container">
+    <div class="title w-full">
       <h1 class="font-black text-4xl text-bold text-nord0 dark:text-nord6">
         Leaderboards
       </h1>
     </div>
-    <router-link
-      :to="to(game.abbreviation)"
-      class="game w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 py-3"
-      v-for="(game, i) in games"
-      :key="i"
+    <div
+      class="games grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
     >
-      <div
-        class="cursor-pointer border-gray-100 rounded overflow-hidden shadow-md"
+      <router-link
+        :to="to(game.abbreviation)"
+        v-for="(game, i) in games"
+        :key="i"
       >
-        <div class="bg-black pb-3/5 relative">
-          <img
-            class="absolute h-full w-full object-cover opacity-75"
-            :src="src(game)"
-            alt="Sunset in the mountains"
-          />
-          <div
-            class="absolute h-full w-full flex items-center justify-center text-center p-2"
-          >
-            <div class="text-gray-100 font-bold">
-              {{ game.names.international }}
+        <div
+          class="cursor-pointer border-gray-100 rounded overflow-hidden shadow-md"
+        >
+          <div class="bg-black pb-3/5 relative">
+            <img
+              class="absolute h-full w-full object-cover opacity-75"
+              :src="src(game)"
+              alt="Sunset in the mountains"
+            />
+            <div
+              class="absolute h-full w-full flex items-center justify-center text-center p-2"
+            >
+              <div class="text-gray-100 font-bold">
+                {{ game.names.international }}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </router-link>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -85,7 +88,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.game:hover {
+.games a:hover {
   transition: all 0.1s ease-in-out;
   transform: scale(1.02);
 }
