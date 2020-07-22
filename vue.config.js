6d4cffb22937daab880b735e25b92cd49c370850
CapitaineToinon <antoinesutter@gmail.com>
2020-07-22 12:51:56 +0200

    publicPath for github

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   productionSourceMap: false,
   css: {
     sourceMap: true
-  }
+  },
+  publicPath: isProd ? '/SpeedSouls/' : '/'
 };
