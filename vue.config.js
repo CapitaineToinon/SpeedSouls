22820f2d4f326bff648da8104c723914e24d5c56
CapitaineToinon <asutter@wideagency.com>
2020-07-23 10:17:39 +0200

    removed old vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
   productionSourceMap: false,
   css: {
     sourceMap: true
   },
-  publicPath: isProd ? '/SpeedSouls/' : '/'
 };
